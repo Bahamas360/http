2bddee38fa8db132b0beacea342a394e4aceda7c
Bahamas360 <mramzan331@gmail.com>
2024-11-18 16:02:53 -0800

    test

---
+++ src/HttpRequest.java
@@ -0,0 +1,13 @@
+import java.net.Socket;
+
+public class HttpRequest implements Runnable {
+    public Socket clientSocket;
+    public HttpRequest(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
